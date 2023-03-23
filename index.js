354dd8bdb330fd55872e26c919558532705242ec
John Louise Lagazo <117743596+thygodfrey@users.noreply.github.com>
2023-03-23 13:26:27 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,36 +4,40 @@ const port = 3000;
 
 // Middleware to inspect x-blokc-key header
 const checkHeader = (req, res, next) => {
-    const header = req.header('x-blokc-key');
-    if (!header) {
-        return res.status(401).send('Unauthorized');
-    }
-    next();
+  const header = req.header('x-blokc-key');
+  if (!header) {
+    return res.status(401).send('Unauthorized');
+  }
+  next();
 }
 
 // Image response handler
-app.get('/image', (req, res) => {
-    res.sendFile(__dirname + '/image.jpg');
+app.get('/image/:id', (req, res) => {
+  const id = req.params.id;
+  if (id < 1 || id > 5) {
+    return res.status(404).send('Not found');
+  }
+  res.sendFile(`${__dirname}/images/${id}.jpg`);
 });
 
 // GET /hello endpoint
 app.get('/hello', (req, res) => {
-    const name = req.query.name || 'world';
-    res.status(200).send(`Hello, ${name}!`);
+  const name = req.query.name || 'world';
+  res.status(200).send(`Hello, ${name}!`);
 });
 
 // POST /hi endpoint
 app.post('/hi', checkHeader, express.json(), (req, res) => {
-    const name = req.body.name || 'world';
-    res.status(200).send(`Hi, ${name}!`);
+  const name = req.body.name || 'world';
+  res.status(200).send(`Hi, ${name}!`);
 });
 
 // Default exception handler
 app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send('Something went wrong!');
+  console.error(err.stack);
+  res.status(500).send('Something went wrong!');
 });
 
 app.listen(port, () => {
-    console.log(`Server is listening at http://localhost:${port}`);
+  console.log(`Server is listening at http://localhost:${port}`);
 });
